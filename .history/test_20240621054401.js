28f6aa6ad1c70f48f981e461e461db9a4f316f87
muteeb waheed <muteebwaheed6@gmail.com>
2024-06-23 02:08:16 -0700

    first commit

---
+++ .history/test_20240621054401.js
@@ -0,0 +1,513 @@
+const array = [
+    
+ {
+    "questions": [
+      {
+        "question": "What is the capital of France?",
+        "options": [
+          {
+            "answerText": "Paris",
+            "isCorrect": true
+          },
+          {
+            "answerText": "London",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Berlin",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Rome",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are primary colors?",
+        "options": [
+          {
+            "answerText": "Red",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Green",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Blue",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Yellow",
+            "isCorrect": true
+          }
+        ]
+      },
+      {
+        "question": "Who wrote the play 'Romeo and Juliet'?",
+        "options": [
+          {
+            "answerText": "William Shakespeare",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Jane Austen",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Charles Dickens",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Leo Tolstoy",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are programming languages?",
+        "options": [
+          {
+            "answerText": "Java",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Banana",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Python",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Orange",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What are the colors of the rainbow?",
+        "options": [
+          {
+            "answerText": "Red",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Green",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Blue",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Yellow",
+            "isCorrect": true
+          }
+        ]
+      },
+      {
+        "question": "Which planet is known as the 'Red Planet'?",
+        "options": [
+          {
+            "answerText": "Mars",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Venus",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Jupiter",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Saturn",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What are the main components of air?",
+        "options": [
+          {
+            "answerText": "Oxygen",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Carbon Dioxide",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Nitrogen",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Argon",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which animals lay eggs?",
+        "options": [
+          {
+            "answerText": "Dogs",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Cats",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Birds",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Frogs",
+            "isCorrect": true
+          }
+        ]
+      },
+      {
+        "question": "What is the capital of Japan?",
+        "options": [
+          {
+            "answerText": "Tokyo",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Beijing",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Seoul",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Bangkok",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are fruits?",
+        "options": [
+          {
+            "answerText": "Carrot",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Apple",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Potato",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Banana",
+            "isCorrect": true
+          }
+        ]
+      },
+      {
+        "question": "Who is credited with inventing the telephone?",
+        "options": [
+          {
+            "answerText": "Thomas Edison",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Alexander Graham Bell",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Nikola Tesla",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Albert Einstein",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are mammals?",
+        "options": [
+          {
+            "answerText": "Dolphin",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Shark",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Salmon",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Octopus",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What is the largest planet in our solar system?",
+        "options": [
+          {
+            "answerText": "Jupiter",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Mars",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Saturn",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Neptune",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are programming languages?",
+        "options": [
+          {
+            "answerText": "Ruby",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Lemon",
+            "isCorrect": false
+          },
+          {
+            "answerText": "C++",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Grapes",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What are the primary colors in painting?",
+        "options": [
+          {
+            "answerText": "Red",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Green",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Blue",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Orange",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are elements on the periodic table?",
+        "options": [
+          {
+            "answerText": "Carbon",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Gold",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Silver",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Ruby",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are European countries?",
+        "options": [
+          {
+            "answerText": "Germany",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Brazil",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Spain",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Australia",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What are the primary colors of light?",
+        "options": [
+          {
+            "answerText": "Red",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Green",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Blue",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Yellow",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are parts of the human digestive system?",
+        "options": [
+          {
+            "answerText": "Stomach",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Lungs",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Brain",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Liver",
+            "isCorrect": true
+          }
+        ]
+      },
+      {
+        "question": "What is the currency of Japan?",
+        "options": [
+          {
+            "answerText": "Yen",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Dollar",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Pound",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Euro",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are elements?",
+        "options": [
+          {
+            "answerText": "Oxygen",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Sun",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Moon",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Earth",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What is the largest ocean on Earth?",
+        "options": [
+          {
+            "answerText": "Pacific Ocean",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Indian Ocean",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Atlantic Ocean",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Arctic Ocean",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "Which of the following are programming languages?",
+        "options": [
+          {
+            "answerText": "Python",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Carrot",
+            "isCorrect": false
+          },
+          {
+            "answerText": "Ruby",
+            "isCorrect": true
+          },
+          {
+            "answerText": "Cucumber",
+            "isCorrect": false
+          }
+        ]
+      },
+      {
+        "question": "What is the chemical symbol for water?",
+        "options": [
+          {
+            "answerText": "H2O",
+            "isCorrect": true
+          },
+          {
+            "answerText": "CO2",
+            "isCorrect": false
+          },
+          {
+            "answerText": "N2",
+            "isCorrect": false
+          },
+          {
+            "answerText": "O2",
+            "isCorrect": false
+          }
+        ]
+      }
+    ]
+  }
+  
+];
+array.forEach();
